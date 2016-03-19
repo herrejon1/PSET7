49f7ad5b1cdf1673a5fe3d735ddbde2d3546945d
herrejon1 <A01154863@itesm.mx>
2016-03-19 15:37:20 -0500

    Create quote_form.php

---
+++ quote_form.php
@@ -0,0 +1,11 @@
+<body style="background-color: lightblue"/>
+<form action="quote.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="symbol" placeholder="Symbol" type="text" style="font-family: Cambria; font-size: 18px"/>
+        </div>
+        <div class="form-group">
+             <button type="submit" class="btn btn-default" style="color: red; font-family: Cambria; font-size: 20px">Submit</button>
+        </div>
+    </fieldset>
+</form>
