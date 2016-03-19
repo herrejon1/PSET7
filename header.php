1145b9f8eb95d3f849cac3aa951527c8a20e97d6
herrejon1 <A01154863@itesm.mx>
2016-03-19 15:36:00 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <!-- http://getbootstrap.com/ -->
+        <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+
+        <link href="/css/styles.css" rel="stylesheet"/>
+
+        <?php if (isset($title)): ?>
+            <title>C$50 Finance: <?= htmlspecialchars($title) ?></title>
+        <?php else: ?>
+            <title>C$50 Finance</title>
+        <?php endif ?>
+
+        <!-- https://jquery.com/ -->
+        <script src="/js/jquery-1.11.3.min.js"></script>
+
+        <!-- http://getbootstrap.com/ -->
+        <script src="/js/bootstrap.min.js"></script>
+
+        <script src="/js/scripts.js"></script>
+
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <div id="top">
+                <div>
+                    <a href="/"><img alt="C$50 Finance" src="/img/logo.png"/></a>
+                </div>
+                <?php if (!empty($_SESSION["id"])): ?>
+                    <ul class="nav nav-pills">
+                        <li><a style= "font-size:14px;" href="index.php"><?= $username ?></a></li>
+                        <li><a href="quote.php">Quote</a></li>
+                        <li><a href="buy.php">Buy</a></li>
+                        <li><a href="sell.php">Sell</a></li>
+                        <li><a href="history.php">History</a></li>
+                        <li role="presentation" class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            <? $name = ["name"] ?> <? $name = ["last_name"] ?><span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                        <li><a href="extra.php">Deposit</a></li>
+                        <li><a href="logout.php"><strong>Log Out</strong></a></li>
+                    </ul>
+                    </li>
+                <?php endif ?>
+            </div>
+
+            <div id="middle">
