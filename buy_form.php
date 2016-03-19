2934bbe2c2132881aac19ad1dcf4b8dab0eef8f1
herrejon1 <A01154863@itesm.mx>
2016-03-19 15:41:48 -0500

    Create buy_form.php

---
+++ buy_form.php
@@ -0,0 +1,14 @@
+<form action="buy.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="symbol" placeholder="Stock" type="text" style="font-family: Futura"/>
+        </div>
+        <div class="form-group">
+            <input autofocus class="form-control" name="shares" placeholder="Shares" type="text" style="font-family: Futura"/>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default" style="font-family: Impact; color: red; font-size: 20px"><b>BUY</b></button>
+        </div>
+        
+    </fieldset>
+</form>
